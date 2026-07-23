ed2c4a33a4e17b93eb9ab2367452d772cff7bb99
Shruti Thakkar <shruti.thakkar@neurosynaptic.com>
2026-07-23 12:25:48 +0530

    parse whole repo using ast

--- app/parser/ast_parser.py
+++ app/parser/ast_parser.py
@@ -21,7 +21,59 @@ def parse_file(filepath:str)-> list[dict]:
     
     print(tree)
     print(ast.dump(tree))
+    
+    class CodeVisitor(ast.NodeVisitor):
+        def __init__(self,source,filepath):
+            self.source= source
+            self.filepath= filepath
+            self.chunks= []
+            self.calls =[]
+            self.current_class= None
+            self.current_function= None
+            
+        def visit_ClassDef(self,node):
+            entry= {
+                "name": node.name,
+                "type": "class",
+                "docstring":ast.get_docstring(node),
+                "source_code": ast.get_source_segment(self.source, node),
+                "file": self.filepath,
+                "start_line":node.lineno,
+                "end_line":node.end_lineno,
+                "parent_class":None
+            }
+            self.chunks.append(entry)
+            
+            prev_class= self.current_class
+            self.current_class = node.name
+            self.generic_visit(node)
+            self.current_class= prev_class
+            
+            def visit_FucntionDef(self,node):
+                entry={
+                            "name": node.name,
+                            "type": "function",
+                            "docstring":ast.get_docstring(node),
+                            "source_code": ast.get_source_segment(self.source, node),
+                            "file": self.filepath,
+                            "start_line":node.lineno,
+                            "end_line":node.end_lineno,
+                            "parent_class":self.current_class
 
+                }
+                self.chunks.append(entry)
+                
+                for call in self.find_calls_in(node):
+                    self.calls.append((node.name, call))
+                
+                prev_function = self.current_function
+                self.current_function = node.name
+                self.generic_visit(node)
+                self.current_function = prev_function
+
+            visit_AsyncFunctionDef = visit_FunctionDef
+            
+        
     results=[]
     for node in ast.walk(tree):
         if isinstance (node, (ast.FunctionDef, ast.AsyncFunctionDef, ast.ClassDef)):
